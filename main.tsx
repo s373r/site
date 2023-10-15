07481b4251be85e1350b0e5af829b052b038c18f
Dima Pristupa <dima@pristupa.dev>
2023-10-15 21:45:21 +0300

    🚨 Fix linter warnings

--- main.tsx
+++ main.tsx
@@ -3,27 +3,32 @@
 import blog, { h } from "blog";
 
 blog({
-    author: "Dima Pristupa",
+  author: "Dima Pristupa",
 
-    avatar: "avatar.png",
-    avatarClass: "rounded-full",
+  avatar: "avatar.png",
+  avatarClass: "rounded-full",
 
-    title: "Dima Pristupa",
-    description: "A Kyiv-based SDE in love with Rust, TypeScript, C++, Python, Haskell and GNU/Linux",
+  title: "Dima Pristupa",
+  description:
+    "A Kyiv-based SDE in love with Rust, TypeScript, C++, Python, Haskell and GNU/Linux",
 
-    links: [
-        { title: "Email", url: "mailto:dima@pristupa.dev" },
-        { title: "LinkedIn", url: "https://www.linkedin.com/in/dima-pristupa/" },
-        { title: "GitHub", url: "https://github.com/s373r" },
-        { title: "CV", url: "https://github.com/s373r", icon: <div className="font-bold">CV</div> },
-    ],
+  links: [
+    { title: "Email", url: "mailto:dima@pristupa.dev" },
+    { title: "LinkedIn", url: "https://www.linkedin.com/in/dima-pristupa/" },
+    { title: "GitHub", url: "https://github.com/s373r" },
+    {
+      title: "CV",
+      url: "https://github.com/s373r",
+      icon: <div className="font-bold">CV</div>,
+    },
+  ],
 
-    // NOTE(DP): Remove when posts are added
-    footer: {},
+  // NOTE(DP): Remove when posts are added
+  footer: {},
 
-    // NOTE(DP): Custom style:
-    style: `
+  // NOTE(DP): Custom style:
+  style: `
 /* Make avatar bigger */
 a.bg-cover { width: 9rem; height: 9rem }
-`
+`,
 });
